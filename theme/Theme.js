b7f32040d214046edf0e91828572c5ad44e4ef11
Kapil Sharma <kapilsharmajmps@gmail.com>
2019-04-08 11:41:07 +0530

    Root Theme initialised at root

---
+++ theme/Theme.js
@@ -0,0 +1,25 @@
+const theme = {
+  // Colors:
+  textDark: '#424242',
+  textLight: '#ffffff',
+  textGrey: '#9e9e9e',
+
+  // BG Colors
+  bgLight: '#ffffff',
+  bgOffWhite: '#f2f2f2',
+  bgGrey: '#9e9e9e',
+
+  // Border
+  borderDefaultColor: '#bdbdbd',
+  borderColorOnFocus: '#9e9e9e',
+  borderErrorColor: '#ff032d',
+
+  // Button
+  buttonBGDark: '#0288d1',
+  buttonBGLight: '#ffffff',
+
+  // Box-Shadow
+  boxShadow: '0 3px 6px 0 rgba(0, 0, 0, 0.16)'
+};
+
+export default theme;
