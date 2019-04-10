e2c52dcfebd541097f4fd7a06c73d8817995f7e0
Kapil Sharma <kapilsharmajmps@gmail.com>
2019-04-10 13:07:56 +0530

    Remaining

---
+++ .storybook/config.js
@@ -0,0 +1,19 @@
+// import { configure } from '@storybook/react';
+
+// // automatically import all files ending in *.stories.js
+// const req = require.context('../stories', true, /\.stories\.js$/);
+// function loadStories() {
+//   req.keys().forEach(filename => req(filename));
+// }
+
+// configure(loadStories, module);
+
+import { configure } from '@storybook/react';
+
+const req = require.context('../components', true, /\.stories\.js$/);
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
